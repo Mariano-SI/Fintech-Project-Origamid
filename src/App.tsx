1d6a24619b9f3c8c1b16c380a60c25edae2bb04f
Mariano Silva <mariano.silva@ufvjm.edu.br>
2024-01-05 19:31:51 -0300

    wip: first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Header from './Components/Header/Header';
+import SideNav from './Components/SideNav/SideNav';
+import Resume from './Pages/Resume/Resume';
 import './Style.css';
 
 function App() {
   return (
     <div>
-
+      <SideNav/>
+      <main>
+        <Header/>
+        <Resume/>
+      </main>
     </div>
   );
 }
---
+++ src/Pages/Resume/Resume.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ResumeStyled } from './Resume.style'
+
+function Resume() {
+  return (
+    <ResumeStyled>
+        Resume
+    </ResumeStyled>
+  )
+}
+
+export default Resume
